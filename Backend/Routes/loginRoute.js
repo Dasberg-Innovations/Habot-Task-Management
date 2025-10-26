ddf083b18b019106ef89146fe70049458f2f0a14
BabyBlob <robloxdog@outlook.com>
2025-10-26 19:46:12 -0400

    Bcrypt Encryption Library Outdated Update

--- Backend/Routes/loginRoute.js
+++ Backend/Routes/loginRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { User } from '../Models/UserLoginModel';
+import { User } from '../Models/UserLoginModel.js';
 import bcrypt from 'bcrypt'; // Encryption for Password
 
 const router = express.Router();

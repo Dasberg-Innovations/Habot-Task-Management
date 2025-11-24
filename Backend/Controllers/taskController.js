9c3facab98733a3099c297f0fce9868c001af7e9
BabyBlob <robloxdog@outlook.com>
2025-11-24 15:59:29 -0400

    Creation of Tasks

--- Backend/Controllers/taskController.js
+++ Backend/Controllers/taskController.js
@@ -8,3 +8,18 @@ const getAllTasks = async (req, res) => {
         res.status(500).json({ message: err.message });
     }
 };
+
+const createTask = async (req, res) => {
+    const task = new Task({
+        username: req.body.username,
+        Task_Title: req.body.Task_Title,
+        Description: req.body.Description,
+        Task_Completed: req.body.Task_Completed || false
+    });
+    try {
+        const newTask = await task.save();
+        res.status(201).json(newTask);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+};
